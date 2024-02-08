78a9588ebc250e13fe1db5390d097a676c1d60e8
putragraha <putra.graha.7@gmail.com>
2024-02-08 13:30:52 +0700

    Create function to play media

--- app/src/main/java/com/nsystem/jetpackmedia3experiment/MainActivity.kt
+++ app/src/main/java/com/nsystem/jetpackmedia3experiment/MainActivity.kt
@@ -10,9 +10,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.nsystem.jetpackmedia3experiment.ui.theme.JetpackMedia3ExperimentTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var playerView: PlayerView
+
+    private val player by lazy {
+        ExoPlayer.Builder(this).build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,10 +33,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AndroidView(
+                        modifier = Modifier.fillMaxSize(),
+                        factory = { context -> PlayerView(context).also { playerView = it } },
+                        update = { playerView ->
+                            playerView.player = player
+                            player.setMediaItem(MediaItem.fromUri(MEDIA_URL))
+                            player.play()
+                        }
+                    )
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        player.release()
+        super.onDestroy()
+    }
+
+    companion object {
+
+        private const val MEDIA_URL = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
     }
 }
 
